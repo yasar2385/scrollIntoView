9166069713c97537c1111d376b858ade946dd9d1
yasar2385 <61082958+yasar2385@users.noreply.github.com>
2023-07-25 18:15:21 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# scrollIntoView
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-g2inbc)
---
+++ index.js
@@ -0,0 +1,20 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+const element = document.getElementById("box");
+
+element.scrollIntoView();
+element.scrollIntoView(false);
+element.scrollIntoView({ block: "end" });
+element.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+
+let btn = document.querySelector('.btn');
+let el = document.querySelector('.special');
+
+btn.addEventListener('click', function () {
+    el.scrollIntoView(true);
+});
