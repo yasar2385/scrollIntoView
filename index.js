4349acfe284902c87bf26752f80d1d769d14c7d8
yasar2385 <61082958+yasar2385@users.noreply.github.com>
2023-07-25 18:23:11 +0530

    3 files modified

--- index.js
+++ index.js
@@ -5,16 +5,33 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
-const element = document.getElementById("box");
+const element = document.getElementById('box');
 
 element.scrollIntoView();
 element.scrollIntoView(false);
-element.scrollIntoView({ block: "end" });
-element.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+element.scrollIntoView({ block: 'end' });
+element.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
 
 let btn = document.querySelector('.btn');
 let el = document.querySelector('.special');
-
+var click = 0;
 btn.addEventListener('click', function () {
+  if (click == 0) {
+    console.log(click);
     el.scrollIntoView(true);
+    click++;
+  } else if (click == 1) {
+    console.log(click);
+    el.scrollIntoView(false);
+    click++;
+  } else if (click == 2) {
+    console.log(click);
+    el.scrollIntoView({ block: 'end' });
+    click++;
+  } else if (click == 3) {
+    console.log(click);
+    el.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
+    click = 0;
+  }
 });
+
